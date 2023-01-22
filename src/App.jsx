4ef934ed39d5633d26f570362a55a305ac9418a1
pak <rupaklamadev@gmail.com>
2023-01-22 13:53:10 -0500

    defined basic css layout rules and created home app component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.heading}>Find Your Personality</header>
+      <main className={styles.main}>
+        <p>hello</p>
+      </main>
+      <footer className={styles.footer}>&#169; Developed by Rupak Lama</footer>
+    </div>
+  );
+};
+
+export default App;
