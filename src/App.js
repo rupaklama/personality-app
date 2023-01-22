c3f3bd4b3428910c42e5cf4352ebdf38fd4b2d73
pak <rupaklamadev@gmail.com>
2023-01-22 13:34:47 -0500

    set up basic config for eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("hi");
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
